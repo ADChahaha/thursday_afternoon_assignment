0070ced006b026af4e14fb338f9851076ccc6497
ADChahaha <3383642310a@gmail.com>
2025-11-28 14:57:55 +0800

    🎉

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, UploadFile, File, Form
+from PIL import Image
+import io
+
+app = FastAPI()
+
+
+@app.post("/detect")
+async def detect(file: UploadFile = File(...), description: str = Form(...)):
+    contents = await file.read()
+    # 转成 BytesIO 对象
+    image_stream = io.BytesIO(contents)
+    
+    # 使用 PIL 打开
+    image = Image.open(image_stream)
+    print(image.width)
+    print(image.height)
+
+
+
