2bbe4627c17a73fa486639dd6e23e225fe5b1a9c
ADChahaha <3383642310a@gmail.com>
2025-11-28 15:47:51 +0800

    🎨修改了eval的实现，支持批量或单张传入PIL.image

--- model/eval.py
+++ model/eval.py
@@ -8,18 +8,49 @@ class ImageProcessor:
     def __init__(self, ckpt_path):
         self.model: torch.nn.Module = MyModel.load_from_checkpoint(ckpt_path)
         self.model.eval()
+        self.img: torch.tensor = None
 
-    def eval(self, img):
-        # img2tensor
-        img : torch.tensor = transforms.ToTensor()(img)
-        # unsqueeze batch_size 1
-        img = img.unsqueeze(dim=0)
-        y = self.model(img)
-        idx = torch.argmax(y, dim=1).item()
-        return self.idx2type(idx)
+    def set_image(self, img):
+        """设置图像用于后续处理
 
-    def idx2type(self, idx):
+        Args:
+            img (list[PIL.image] | PIL.image): PIL图像或PIL图像列表
+        """
+        # 转换为tensor且提升batch为1
+        transform = transforms.ToTensor()
+        if isinstance(img, list):
+            tensors = [transform(i) for i in img]
+            self.img = torch.stack(tensors, dim=0)
+        else:
+            tensor = transform(img)
+            self.img = tensor.unsqueeze(dim=0)
+
+    def eval(self):
+        """将内部的image全部识别
+
+        Returns:
+            list[str]
+        """
+        y = self.model(self.img)  # (batch_size, num_class)
+        idx = torch.argmax(y, dim=1)  # idx shape: (batch_size,)
+        return [self.__idx2type__(i.item()) for i in idx]
+
+    def cam(self):
+        # 待实现
+        pass
+
+    def __idx2type__(self, idx):
+        """将模型识别的idx转换为文本
+
+        Args:
+            idx (int): 模型生成的idx
+
+        Returns:
+            str: 下标对应的文本描述
+        """
         if 0 == idx:
             return "HUMAN"
         elif 1 == idx:
             return "AI"
+        else:
+            return "UNKNOWN"
