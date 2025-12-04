060b525973afa5d614cd8630d996180204f6a618
ADChahaha <3383642310a@gmail.com>
2025-12-04 15:48:12 +0800

    跑通代码，cam图实现，修复前端缓存bug

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import io
 import sys
 from pathlib import Path
 import os
+import base64
 
 # 添加model搜索路径
 sys.path.append(str((Path(__file__).parent / "..").resolve() / "model"))
@@ -12,12 +13,18 @@ from eval import ImageProcessor
 # 读取.env
 from dotenv import load_dotenv
 
-load_dotenv(Path(__file__).parent.parent / ".env")
-processor = ImageProcessor(os.getenv("CKPT_PATH"))
+base_path = Path(__file__).parent.parent
+load_dotenv(base_path / ".env")
+processor = ImageProcessor(os.path.join(base_path, os.getenv("CKPT_PATH")))
 
 # 创建fastapi实例
 app = FastAPI()
 
+def pil_to_base64(img: Image.Image) -> str:
+    """PIL.Image -> base64 string"""
+    buffered = io.BytesIO()
+    img.save(buffered, format="JPEG")
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
 
 @app.post("/detect")
 async def detect(file: UploadFile = File(...)) -> dict:
@@ -27,7 +34,7 @@ async def detect(file: UploadFile = File(...)) -> dict:
         file (UploadFile, optional): Assume to be an image with width and length > 0
 
     Returns:
-        return: {"type": , "cam": } 
+        return: {"type": , "cam": }
     """
     contents = await file.read()
     # 转成 BytesIO 对象
@@ -43,7 +50,8 @@ async def detect(file: UploadFile = File(...)) -> dict:
     # 对于只有一个图片的情况，直接返回type[0]
     if 1 == len(type):
         type = type[0]
-    if 1 == len(cam):
-        cam = cam[0]
-
+    if len(cam) == 1:
+        cam = pil_to_base64(cam[0])
+    else:
+        cam = [pil_to_base64(c) for c in cam]
     return {"type": type, "cam": cam}
